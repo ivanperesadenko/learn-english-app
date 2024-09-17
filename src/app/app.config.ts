d4f3ce1ebc90757cbf4d7cf3a2099177594b9508
Ivan Peresadenko <ivanperesadenkoo@gmail.com>
2024-09-17 22:58:12 +0300

    NgXs basic setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,16 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
+import { provideStore as provideNgXsStore } from '@ngxs/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideRouterStore } from '@ngrx/router-store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { withNgxsReduxDevtoolsPlugin } from '@ngxs/devtools-plugin';
+import { withNgxsFormPlugin } from '@ngxs/form-plugin';
+// import { withNgxsLoggerPlugin } from '@ngxs/logger-plugin';
+import { withNgxsRouterPlugin } from '@ngxs/router-plugin';
+// import { withNgxsStoragePlugin } from '@ngxs/storage-plugin';
+// import { withNgxsWebSocketPlugin } from '@ngxs/websocket-plugin';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -17,5 +24,14 @@ export const appConfig: ApplicationConfig = {
 		provideEffects(),
 		provideRouterStore(),
 		provideStoreDevtools({ maxAge: 50, logOnly: !isDevMode() }),
+		provideNgXsStore(
+			[],
+			withNgxsReduxDevtoolsPlugin(),
+			withNgxsFormPlugin(),
+			// withNgxsLoggerPlugin(),
+			withNgxsRouterPlugin()
+			// withNgxsStoragePlugin({  }),
+			// withNgxsWebSocketPlugin()
+		),
 	],
 };
